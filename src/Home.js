c428f4ec89eefcad19a314306ec1aa65cc9c192e
ankus <iamankus7@gmail.com>
2021-02-13 17:48:01 +0530

    define function in components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello Ankus",e)
+    }
+    const handleClickAgain = (name,e) => {
+        console.log(`hello ${name}`,e.target)
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Ankus',e)}>Click me again</button>
         </div>
      );
 }
